ec263da089567ad71150d8f932b14b34328b9b3e
Afsanefda <afsoonfadaei@gmail.com>
2020-08-09 22:04:26 +0430

    :construction_worker: format circle config added with errors

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,25 +3,23 @@ import React from 'react';
 import logo from '../../logo.svg';
 import './index.css';
 
-const Home = ({title})  => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {title}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Home = ({ title }) => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        {title}
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 
 export default Home;
